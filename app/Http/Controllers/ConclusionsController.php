9d008885e7e553fb518b591e8b3e056ae0f35bfe
AALvov <Prostoparol228>
2023-02-13 16:48:52 +0300

    Переход с ЖАС на элемент + фильтр в справочнике ДО

--- app/Http/Controllers/ConclusionsController.php
+++ app/Http/Controllers/ConclusionsController.php
@@ -4,6 +4,7 @@ namespace App\Http\Controllers;
 
 use App\Conclusions_industrial_safety;
 use App\Models\Main_models\RefDO;
+use App\Models\Main_models\RefOpo;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Storage;
@@ -23,7 +24,7 @@ class ConclusionsController extends Controller
         return view('web.docs.reports.conclusions_industrial_safety_main', compact('i', 'page', 'data_one'));
     }
 
-    public function get_group_conclusion(Request $request, $column)
+    public function get_group_conclusion(Request $request, $table, $column)
     {
         if ($request->all()) {
             $keys = array_keys($request->all());
@@ -32,12 +33,30 @@ class ConclusionsController extends Controller
                 if (isset($data_one)) {
                     $data_one = $data_one->whereIn($key, $fieldset[$key]);
                 } else {
-                    $data_one = Conclusions_industrial_safety::whereIn($key, $fieldset[$key]);
+                    switch ($table) {
+                        case 'conclusions':
+                            $data_one = Conclusions_industrial_safety::whereIn($key, $fieldset[$key]);
+                            break;
+                        case 'ref_do':
+                            $data_one = DB::table('public.ref_do')->
+                            join('public.typestatus', 'public.ref_do.id_status', '=', 'public.typestatus.id_status')->whereIN($key, $fieldset[$key]);
+                            break;
+                    }
                 }
             }
             $data_one->groupby($column)->orderby($column);
         } else {
-            $data_one = Conclusions_industrial_safety::groupby($column)->orderby($column);
+            switch ($table) {
+                case 'conclusions':
+                    $data_one = Conclusions_industrial_safety::groupby($column)->orderby($column);
+                    break;
+                case 'ref_do':
+                    $data_one = DB::table('public.ref_do')->
+                    join('public.typestatus', 'public.ref_do.id_status', '=', 'public.typestatus.id_status')
+                        ->groupby($column)->orderby($column);
+                    break;
+            }
+
         }
         return $data_one->get($column);
     }
--- resources/views/web/docs/infoDO/index.blade.php
+++ resources/views/web/docs/infoDO/index.blade.php
@@ -10,6 +10,47 @@
     @endcan
 
     @include('web.include.sidebar_doc')
+    <style>
+        label {
+            font: 14px 'Fira Sans', sans-serif;
+        }
+
+        .checkbox {
+            float: left;
+            width: 90%;
+            text-align: left;
+        }
+
+        input {
+            margin: 0.4rem;
+        }
+
+        fieldset {
+            position: absolute;
+            width: 250px;
+            height: 500px;
+            right: -240px;
+            bottom: -510px;
+            background-color: white;
+            z-index: 30;
+            padding: 3px;
+            overflow-y: auto
+        }
+
+        .img {
+            position: absolute;
+            right: 0px;
+            bottom: 0px;
+            width: 20px;
+            border: 2px solid white;
+            border-radius: 2px;
+            background-color: white
+        }
+
+        .img:hover {
+            border: 2px solid darkgray;
+        }
+    </style>
     @can('events-view')
         <div class="top_table">
             @include('web.include.toptable')
@@ -17,7 +58,8 @@
     @endcan
     <div class="inside_content">
 
-        <div class="card-header" style="margin-top: 30px"><h2 class="text-muted" style="text-align: center">Справочник филиалов
+        <div class="card-header" style="margin-top: 30px"><h2 class="text-muted" style="text-align: center">Справочник
+                филиалов
                 ДО</h2></div>
         <div class="doc_header" style="padding-bottom: 6px">
             <table>
@@ -42,18 +84,18 @@
                     <thead>
                     <tr>
                         <th style="width: 5%">Номер</th>
-                        <th style="width: 25%">Краткое наименование филиала ДО</th>
-                        <th style="width: 45%">Наименование филиала ДО</th>
-                        <th style="width: 10%">Регион</th>
-                        <th style="width: 10%">Состояние</th>
-                                                @can('entries-edit')
-                                                <th style="width: 5%"></th>
-                                                @endcan
+                        <th style="width: 25%" class="filter short_name_do">Краткое наименование филиала ДО</th>
+                        <th style="width: 45%" class="filter full_name_do">Наименование филиала ДО</th>
+                        <th style="width: 10%" class="filter region">Регион</th>
+                        <th style="width: 10%" class="filter descstatus">Состояние</th>
+                        @can('entries-edit')
+                            <th style="width: 5%"></th>
+                        @endcan
                     </tr>
 
                     </thead>
                     <tbody>
-                    <?php $i =1 ?>
+                    <?php $i = 1 ?>
                     @foreach ($opos as $row)
                         <tr>
                             <td style="text-align: center">{{ $i }}</td>
@@ -64,13 +106,13 @@
                             <td class="centered" style="text-align: center">
 
                                 <a href="/docs/directory_do/edit/{{$row->id_do}}"><img alt=""
-                                                                                         src="{{asset('assets/images/icons/edit.svg')}}"
-                                                                                         class="check_i"
-                                                                                         style=" margin-left: 10px; margin-right: 5px"></a>
+                                                                                       src="{{asset('assets/images/icons/edit.svg')}}"
+                                                                                       class="check_i"
+                                                                                       style=" margin-left: 10px; margin-right: 5px"></a>
                                 <a href="/docs/directory_do/show/{{$row->id_do}}"><img alt=""
                                                                                        src="{{asset('assets/images/icons/search.svg')}}"
                                                                                        class="check_i"
-                                                                                       ></a>
+                                    ></a>
                             </td>
                         </tr>
                         <?php $i++ ?>
@@ -86,6 +128,198 @@
         input.oninput = function () {
             setTimeout(find_it, 100);
         };
+        let img;
+        {{--<img class="img" alt=""--}}
+        {{--     src="{{asset('assets/images/icons/arrow_bottom.svg')}}"--}}
+        {{--     onclick="get_group_conclusion('center_name', this.parentNode); hide_all_field('fieldsheet_center_name')">--}}
+        document.querySelectorAll('.filter').forEach((el) => {
+            img = document.createElement('img');
+            img.classList.add('img');
+            img.src = "{{asset('assets/images/icons/arrow_bottom.svg')}}";
+            img.onclick = function () {
+                get_group_conclusion(el.classList[1], el);
+                hide_all_field('fieldsheet_' + el.classList[1])
+                console.log(el.classList[1]);
+            }
+            el.append(img)
+        })
+
+        function get_group_conclusion(column, th) {
+            if (!document.getElementById('fieldsheet_' + column)) {
+                let params = {};
+                let fieldsheets = document.getElementsByTagName('fieldset');
+                for (var fieldsheet of fieldsheets) {
+                    var check_input_all = fieldsheet.getElementsByTagName('input')
+                    var check_input = []
+                    var all_input_checked = true
+                    for (var one_input of check_input_all) {
+                        if (one_input.hasAttribute('checked')) {
+                            check_input.push(one_input.getAttribute('name'))
+                        } else {
+                            all_input_checked = false
+                        }
+                    }
+                    params[fieldsheet.id.replace('fieldsheet_', '')] = check_input.join(',');
+                }
+                console.log(params)
+                $.ajaxSetup({
+                    headers: {
+                        'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+                    }
+                });
+                $.ajax({
+                    url: '/get_group_conclusion/' + 'ref_do/' + column,
+                    type: 'POST',
+                    data: params,
+                    success: (res) => {
+                        var fieldset = document.createElement('fieldset')
+                        fieldset.id = 'fieldsheet_' + column
+                        fieldset.innerHTML += `<div class="doc_header" style="padding: 0px; width: 100%;  top: 0px; position: sticky">
+                                                <input type="text" id="search_fieldsheet_${column}" style="margin: 9px 0px" placeholder="Поиск..." oninput="find_field('fieldsheet_${column}')">
+                                                <div class="bat_add" style="margin-left: 0px;">
+                                                    <a
+                                                        onclick="get_data()"
+                                                        style="display: inline-block; margin-left: 0px">Применить</a>
+                                                    <a class="on_off_btn"
+                                                        onclick="checked('fieldsheet_${column}')"
+                                                        style="display: inline-block; margin-left: 0px">Вкл/выкл все</a>
+                                                </div>
+                                            </div>`
+                        for (var row of res) {
+                            fieldset.innerHTML += `<div class="checkbox">
+                                            <input type="checkbox" class="checkbox_button" name="${row[column]}" checked>
+                                            <label for="${row[column]}">${row[column]}</label>
+                                         </div>`
+                        }
+                        th.appendChild(fieldset)
+                        checked()
+                    }
+                })
+            }
+        }
+
+        function hide_all_field(id) {
+            for (var field of document.getElementsByTagName('fieldset')) {
+                if (field.id == id) {
+                    if (field.style.display === 'none') {
+                        field.style.display = ''
+                    } else {
+                        field.style.display = 'none'
+                    }
+                } else {
+                    field.style.display = 'none'
+                    field.getElementsByClassName('on_off_btn')[0].onclick = ''
+                    for (var input of field.getElementsByTagName('input')) {
+                        input.setAttribute('disabled', true)
+                    }
+                }
+            }
+        }
+
+        function checked(id) {
+            if (id) {
+                var true_button = false
+                var checkboxes = document.getElementById(id).getElementsByClassName('checkbox_button')
+                for (var check of checkboxes) {
+                    if (check.hasAttribute('checked')) {
+                        true_button = true
+                    }
+                }
+                if (true_button) {
+                    for (var check of checkboxes) {
+                        check.removeAttribute('checked')
+                    }
+                } else {
+                    for (var check of checkboxes) {
+                        check.setAttribute('checked', true)
+                    }
+                }
+            } else {
+                for (var check of document.getElementsByClassName('checkbox_button')) {
+                    check.addEventListener('click', function () {
+                        if (this.hasAttribute('checked')) {
+                            this.removeAttribute('checked')
+                        } else {
+                            this.setAttribute('checked', true)
+                        }
+                    })
+                }
+            }
+        }
+
+        function get_data() {
+            var fieldsheets = document.getElementsByTagName('fieldset')
+            var data = {}
+            for (var fieldsheet of fieldsheets) {
+                var check_input_all = fieldsheet.getElementsByTagName('input')
+                var check_input = []
+                var all_input_checked = true
+
+                for (var one_input of check_input_all) {
+                    if (one_input.hasAttribute('checked')) {
+                        check_input.push(one_input.getAttribute('name'))
+                    } else {
+                        all_input_checked = false
+                    }
+                }
+                console.log(check_input.join(','))
+
+                // input_form.name = fieldsheet.id.replace('fieldsheet_', '')
+                // input_form.type = 'text'
+                // input_form.value = check_input
+                data[fieldsheet.id.replace('fieldsheet_', '')] = check_input.join(',')
+
+                // post_form.appendChild(input_form)
+            }
+            console.log(data)
+            $.ajax({
+                url: '/docs/get_do_data',
+                method: 'POST',
+                data: data,
+                success(res) {
+                    let tbody = document.getElementById('table_for_search').getElementsByTagName('tbody')[0];
+                    tbody.innerHTML = '';
+                    let i = 1
+                    for (var row of res) {
+                        var tr = document.createElement('tr')
+                        tr.innerHTML += `<td style="text-align: center"><p style="margin: 0; display: inline; ">${i}</p></td>`
+                        tr.innerHTML += `<td style="text-align: center"><p style="margin: 0; display: inline; ">${row['short_name_do']}</p></td>`
+                        tr.innerHTML += `<td style="text-align: center"><p style="margin: 0; display: inline; ">${row['full_name_do']}</p></td>`
+                        if (row['region']) {
+                            tr.innerHTML += `<td style="text-align: center"><p style="margin: 0; display: inline; ">${row['region']}</p></td>`
+
+                        } else {
+                            tr.innerHTML += `<td style="text-align: center"><p style="margin: 0; display: inline; "></p></td>`
+
+                        }
+
+                        tr.innerHTML += `<td style="text-align: center"><p style="margin: 0; display: inline; ">${row['descstatus']}</p></td>`
+
+
+                        tr.innerHTML += ` @can('report-edit') <td style="text-align: center; min-width: auto">
+                     <a href="/docs/directory_do/edit/${row['id_do']}"><img alt=""
+                                                                                         src="{{asset('assets/images/icons/edit.svg')}}"
+                                                                                         class="check_i"
+                                                                                         style=" margin-left: 10px; margin-right: 5px"></a>
+                                <a href="/docs/directory_do/show/${row['id_do']}"><img alt=""
+                                                                                       src="{{asset('assets/images/icons/search.svg')}}"
+                                                                                       class="check_i"
+                                                                                       ></a>
+                    </td> @endcan`
+                        tbody.appendChild(tr)
+
+                        i++
+                    }
+                },
+                error: function (error) {
+                    var table_body = document.getElementById('body_table')
+                    table_body.innerText = ''
+                },
+
+
+            })
+
+        }
 
         function find_it() {
             var table_boby_rows = document.getElementById('table_for_search').getElementsByTagName('tbody')[0].getElementsByTagName('tr')  //строки по которым ищем
